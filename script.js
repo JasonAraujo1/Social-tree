5e438169a404129932d97e849eb79af3985c28ce
Jason A. <you@example.com>
2023-09-26 22:14:16 -0300

    hover buttom links fix

--- script.js
+++
@@ -1,8 +0,0 @@
-// Selecione o botão pelo seu ID
-const meuBotao = document.getElementById("meuBotao");
-
-// Adicione um ouvinte de eventos para o evento de clique
-meuBotao.addEventListener("click", function() {
-    // Adicione ou remova a classe "clicado" quando o botão for clicado
-    this.classList.toggle("clicado");
-});
